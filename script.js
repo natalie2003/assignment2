4dbf743f59224188a55d7b4318cfb5e89e8a8e78
Natalie Long <natalie.long1818@gmail.com>
2021-01-09 15:10:21 +0800

    add login details

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+// $(document).ready(function){
+//   var 
+// }
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+
+function register(){
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.style.left = "110px";
+}
+
+function login(){
+  x.style.left = "50px";
+  y.style.left = "450px";
+  z.style.left = "0";
+}
+
+function Member(username, email, password){
+  this.username = username;
+  this.email = email;
+  this.password = password;
+}
+
+//base and store info from input
+
+let memberList = JSON.parse()
